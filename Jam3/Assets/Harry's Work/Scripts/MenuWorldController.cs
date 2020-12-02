f06d9ae3cf9c2d4da36e8bc30a81f77e653c91a8
Harry <harry3.newton@live.uwe.ac.uk>
2020-12-02 22:40:47 +0000

    Selections and Ghosts

---
+++ Jam3/Assets/Harry's Work/Scripts/MenuWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWorldController : MonoBehaviour
+{
+    [Header("World Setup")]
+    public GameObject ghost;
+    public GameObject[] spawns;
+    public float spawnTestInterval;
+    public int spawnChance;
+
+    private void Start() 
+    {
+        InvokeRepeating("SpawnGhost",0, spawnTestInterval);
+    }
+
+    void SpawnGhost()
+    {
+        Debug.Log("Test");
+        if(Random.Range(0, 100) <= spawnChance)
+        {
+            int spawnSelect = Random.Range(0, spawns.Length);
+
+            Instantiate(ghost, spawns[spawnSelect].transform.position, Quaternion.identity);
+        }
+    }   
+}
