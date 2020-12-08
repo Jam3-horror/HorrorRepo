a5a5f7c672e93f6d337aa5c08010083a34c5e3de
Jake <jake3.powell@live.uwe.ac.uk>
2020-12-08 14:47:28 +0000

    added some events

---
+++ Jam3/Assets/Scenes/Levels/Trigger scripts/TvTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TvTrigger : MonoBehaviour
+{
+
+    public Light tvLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tvLight.intensity = Random.Range(5f, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tvLight.GetComponent<AudioSource>().Stop();
+            Destroy(tvLight);
+        }
+    }
+}
