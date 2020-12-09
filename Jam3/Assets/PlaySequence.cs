7d0a584a3bf6b2e7d68ba07306b1c1caa05be865
Harry <harry3.newton@live.uwe.ac.uk>
2020-12-09 05:47:13 +0000

    Audio puzzle done
    JUST WANT TO GO TO BED FUCK OFF

---
+++ Jam3/Assets/PlaySequence.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySequence : MonoBehaviour
+{
+    public float activationRadius;
+    public GameObject fText;
+    public AudioClip[] clips;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, activationRadius);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Player"))
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    StartCoroutine("Play");
+                }
+            }
+        }
+    }
+
+    IEnumerator Play()
+    {
+        for (int i = 0; i < clips.Length; i++)
+        {
+            audioSource.clip = clips[i];
+            audioSource.Play();
+
+            yield return new WaitForSeconds(clips[i].length);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fText.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, activationRadius);
+    }
+}
