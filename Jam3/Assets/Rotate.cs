b4de1c01d15d041efbae7b898a4c901bdb3a8076
Harry <harry3.newton@live.uwe.ac.uk>
2020-12-09 03:39:31 +0000

    Bedroom 2 done, bathroom puzzle beginning

---
+++ Jam3/Assets/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime, 0, 0);
+    }
+}
