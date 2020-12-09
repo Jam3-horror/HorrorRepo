e710a938e3e60c0367f7cb4b3dc2eea86d13b78c
Joseph <josephgarrad2017@gmail.com>
2020-12-09 06:18:56 +0000

    added win lose menus

---
+++ Jam3/Assets/LoseMen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseMen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void restartGame()
+    {
+        SceneManager.LoadScene("Hotel");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
